be77b27102478dc8c5f004c2a1fe6a2eb28e0522
davebulaval <david.beauchemin.5@ulaval.ca>
2020-08-24 17:55:51 -0400

    clearer code

--- deepParse/model/encoder.py
+++ deepParse/model/encoder.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torch.nn.utils.rnn import pack_padded_sequence
 
 from deepParse.tools import weight_init
 
@@ -16,20 +17,23 @@ class Encoder(nn.Module):
         self.lstm.apply(weight_init)
 
     def forward(self, input_, lenghts_tensor):
-        if input_.size(0) < self.batch_size: # @Marouane à quoi cela sert-il ? On ne peut pas jsute le faire dans l'init ? 
-            batch_size = 1                  # C'est pour pouvoir gérer des batch sizes différents de façon dynamique (e.g: l'entrainement du modèle a été fait avec batch size de 2048)
-            self.hidden = self.__init_hidden(batch_size, self.hidden_size) # mais il y'a des pays qui ont moins que 2048 données au total (penses au pays zero shot) 
-        else:                                                               # donc si on fixe ça dans l'init ça plante
-            self.hidden = self.__init_hidden(self.batch_size, self.hidden_size) # Ça va être utile pour le parser aussi car je veux garder la possibilité de traitement en batch
-                                                                                # pendant l'inférence comme ça si un user a un grand nombre d'adresses on pourra optimiser
-        packed_sequence = nn.utils.rnn.pack_padded_sequence(input_, lenghts_tensor, batch_first=True)
+        self._dynamic_init_hidden(input_)
 
-        _ , hidden = self.lstm(packed_sequence, self.hidden)
+        packed_sequence = pack_padded_sequence(input_, lenghts_tensor, batch_first=True)
+
+        _, hidden = self.lstm(packed_sequence, self.hidden)
 
         return hidden
 
-    def __init_hidden(self, batch_size, hidden_size):
-        return (torch.zeros(1, batch_size, hidden_size).to(self.device), 
+    def _dynamic_init_hidden(self, input_):
+        if input_.size(0) < self.batch_size:
+            batch_size = 1
+            self.hidden = self._init_hidden(batch_size,
+                                            self.hidden_size)
+        else:
+            self.hidden = self._init_hidden(self.batch_size,
+                                            self.hidden_size)
+
+    def _init_hidden(self, batch_size, hidden_size):
+        return (torch.zeros(1, batch_size, hidden_size).to(self.device),
                 torch.zeros(1, batch_size, hidden_size).to(self.device))
-
-

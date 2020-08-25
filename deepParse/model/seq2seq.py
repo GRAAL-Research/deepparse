8afa1a0a77d32c23a3220faa1a2fc6890f09b90c
davebulaval <david.beauchemin.5@ulaval.ca>
2020-08-25 15:43:21 -0400

    updated version of seq2seq and address parser -unfinish

--- deepParse/model/seq2seq.py
+++ deepParse/model/seq2seq.py
@@ -1,5 +1,5 @@
-import random
-from abc import ABC, abstractmethod
+import os
+from abc import ABC
 from typing import Union
 
 import torch
@@ -12,111 +12,83 @@ from deepParse.model.embedding_network import EmbeddingNetwork
 
 class PretrainedSeq2SeqModel(ABC, nn.Module):
     """
-    Abstract class for callable #todo .
+    Abstract class for callable pre trained Seq2Seq model. The model use the pre trained config for the encoder and
+    decoder:
+        - Encoder: `input_size = 600`, `hidden_size = 600` and `num_layers = 1`
+        - Decoder: `input_size = 1`, `hidden_size = 600`, `num_layers = 1` and `output_size = 9` (the number of tags)
+
+     Args:
+        device (str): The device tu use for the prediction, can either be a GPU or a CPU.
     """
 
-    def __init__(self, device: Union[int, str], batch_size: Union[int, None] = None) -> None:
+    def __init__(self, device: str) -> None:
         super().__init__()
-        self.device = "cuda:%d" % str(device)
-        self.batch_size = 1 if batch_size is None else batch_size
+        self.device = device
 
-        self.output_size = 1  # todo use pre trained size
+        self.encoder = Encoder(input_size=600, hidden_size=600, num_layers=1)
 
-        self.encoder = Encoder(1, 1, 1, self.batch_size)
-        self.encoder.to(device)
+        self.decoder = Decoder(input_size=1, hidden_size=600, num_layers=1, output_size=9)
 
-        self.decoder = Decoder(1, 1, 1, 1)
-        self.decoder.to(device)
-
-    @abstractmethod
-    def __call__(self, to_predict, lenghts_tensor):
+    def _load_pre_trained_weights(self, model_type: str):  # todo
+        # todo get input and output
+        path = os.path.join(os.path.expanduser('~'), "deepParse_data")
+        os.makedirs(path, exist_ok=True)
         pass
 
-    def _get_dynamic_batch(self, input_):
-        return 1 if input_.size(0) < self.batch_size else self.batch_size
-
-
-# pretrained class
-class FastTextAddressSeq2SeqModel(PretrainedSeq2SeqModel):
-    def __init__(self, device: Union[int, str], batch_size: Union[int, None] = None) -> None:
-        super().__init__(device, batch_size)
-
-    def __call__(self, to_predict, lenghts_tensor):
-        batch_size = self._get_dynamic_batch(to_predict)
-
-        decoder_input, decoder_hidden = self._encoder_step(to_predict, lenghts_tensor, batch_size)
-
-        decoder_output, _ = self.decoder(decoder_input, decoder_hidden)
-
-        return decoder_output
-
-    def _encoder_step(self, input_, lenghts_tensor, batch_size):
-        decoder_hidden = self.encoder(input_, lenghts_tensor)
+    def _encoder_step(self, to_predict, lenghts_tensor, batch_size):  # todo get input and output
+        decoder_hidden = self.encoder(to_predict, lenghts_tensor)
 
         # -1 for BOS token
-        decoder_input = torch.zeros(1, batch_size, 1).cuda(self.device).new_full((1, batch_size, 1), -1)
+        decoder_input = torch.zeros(1, batch_size, 1).to(self.device).new_full((1, batch_size, 1), -1)
         return decoder_input, decoder_hidden
 
 
-class BPEmbAddressSeq2SeqModel(nn.Module):
-    def __init__(self, encoder_input_size, decoder_input_size, encoder_hidden_size, num_encoding_layers,
-                 decoder_hidden_size, num_decoding_layers, output_size, batch_size, EOS_token, device):
-        super().__init__()
-        self.output_size = output_size
-        self.batch_size = batch_size
-        self.device = device
+# pretrained class
+class PretrainedFastTextSeq2SeqModel(PretrainedSeq2SeqModel):
+    """
+    FastText pre trained Seq2Seq model, the lightest of the two (in GPU/CPU consumption) for a little less accuracy.
 
-        self.embedding_network = EmbeddingNetwork(embedding_input_size, embedding_hidden_size,
-                                                  embedding_projection_size, self.batch_size, device)
-        self.embedding_network.to(device)
+     Args:
+        device (str): The device tu use for the prediction, can either be a GPU or a CPU.
+    """
 
-        self.encoder = Encoder(encoder_input_size, encoder_hidden_size, num_encoding_layers, self.batch_size, device)
-        self.encoder.to(device)
+    def __init__(self, device: Union[int, str]) -> None:
+        super().__init__(device)
 
-        self.decoder = Decoder(decoder_input_size, decoder_hidden_size, num_decoding_layers, output_size,
-                               self.batch_size, device)
-        self.decoder.to(device)
+        self._load_pre_trained_weights("FastText")
 
-        self.EOS_token = EOS_token
+    def __call__(self, to_predict, lenghts_tensor):
+        batch_size = to_predict.size(0)
 
-    def forward(self, input_, decomposition_lengths, lenghts_tensor, target=None):
-        embedded_input = self.embedding_network(input_.to(self.device), decomposition_lengths)
+        decoder_input, decoder_hidden = self._encoder_step(to_predict, lenghts_tensor, batch_size)
 
-        if embedded_input.size(0) < self.batch_size:
-            batch_size = 1
-        else:
-            batch_size = self.batch_size
+        decoder_predict = self.decoder(decoder_input, decoder_hidden)
 
-        encoder_hidden = self.encoder(embedded_input.to(self.device), lenghts_tensor)
+        return decoder_predict
 
-        decoder_hidden = encoder_hidden
-        max_length = lenghts_tensor[0].item()
 
-        output_sequence = torch.zeros(max_length + 1, batch_size, self.output_size).to(self.device)
+class PretrainedBPEmbSeq2SeqModel(PretrainedSeq2SeqModel):
+    """
+    BPEmb pre trained Seq2Seq model, the best of the two, but take the more GPU/CPU resources.
 
-        BOS_token = -1
-        decoder_input = torch.zeros(1, batch_size, 1).to(self.device).new_full((1, batch_size, 1), BOS_token)
+     Args:
+        device (str): The device tu use for the prediction, can either be a GPU or a CPU.
+    """
 
-        decoder_output, decoder_hidden = self.decoder(decoder_input, decoder_hidden)
+    def __init__(self, device: Union[int, str]):
+        super().__init__(device)
 
-        output_sequence[0] = decoder_output
+        self.embedding_network = EmbeddingNetwork(input_size=300, hidden_size=300)
 
-        _, decoder_input = decoder_output.topk(1)
+        self._load_pre_trained_weights("BPEmb")
 
-        if not target is None and random.random() < 0.5:
-            target = target.transpose(0, 1)
-            for idx in range(max_length):
-                decoder_input = target[idx].view(1, batch_size, 1)
-                decoder_output, decoder_hidden = self.decoder(decoder_input, decoder_hidden)
+    def __call__(self, to_predict, lenghts_tensor, decomposition_lengths):
+        batch_size = to_predict.size(0)
 
-                output_sequence[idx + 1] = decoder_output
+        embedded_output = self.embedding_network(to_predict, decomposition_lengths)
 
-        else:
-            for idx in range(max_length):
-                decoder_output, decoder_hidden = self.decoder(decoder_input.view(1, batch_size, 1), decoder_hidden)
+        decoder_input, decoder_hidden = self._encoder_step(embedded_output, lenghts_tensor, batch_size)
 
-                output_sequence[idx + 1] = decoder_output
+        decoder_predict = self.decoder(decoder_input, decoder_hidden)
 
-                _, decoder_input = decoder_output.topk(1)
-
-        return output_sequence
+        return decoder_predict
--- deepParse/parser/address_parser.py
+++ deepParse/parser/address_parser.py
@@ -1,10 +1,13 @@
 import os
 from json import load
+from typing import List, Union
 
-from deepParse.converter.converter import TargetConverter
-from deepParse.embeddings_model import FastTextEmbeddingsModel, bp_embeddings_model
-from deepParse.model import FastTextAddressSeq2SeqModel, BPEmbAddressSeq2SeqModel
-from deepParse.tools import download_model
+from deepParse.converter import TagsConverter, data_padding
+from deepParse.converter.data_padding import bpemb_data_padding
+from deepParse.embeddings_model import FastTextEmbeddingsModel
+from deepParse.embeddings_model.bp_embeddings_model import BPEmbEmbeddingsModel
+from deepParse.model import PretrainedFastTextSeq2SeqModel, PretrainedBPEmbSeq2SeqModel
+from deepParse.tools import download_fasttext_model
 from deepParse.vectorizer import FastTextVectorizer, BPEmbVectorizer
 
 
@@ -13,29 +16,51 @@ class AddressParser:
     **For fastText, will download data in deepParse_data first time not seen in user root.
     """
 
-    def __init__(self, model, device):
-        pre_trained_tags = load(open("pre_trained_tags_to_idx.json", "r"))
-        target_converter = TargetConverter(pre_trained_tags)
+    def __init__(self, model: str, device: Union[int, str]):
+        self.device = "cuda:%d" % str(device)
+
+        self.tags_converter = TagsConverter(load(open("pre_trained_tags_to_idx.json", "r")))
 
         if model == "fasttext" or model == "lightest":
-            path = os.path.join(os.path.expanduser('~'), "deepParse_data")  # todo to validate if work
+            path = os.path.join(os.path.expanduser('~'), "deepParse_data")
             os.makedirs(path, exist_ok=True)
 
-            file_name = download_model("fr", saving_dir=path)
+            file_name = download_fasttext_model("fr", saving_dir=path)
             embeddings_model = FastTextEmbeddingsModel(file_name)
 
-            self.vectorizer = FastTextVectorizer(embeddings_model=embeddings_model, target_converter=target_converter)
+            self.vectorizer = FastTextVectorizer(embeddings_model=embeddings_model)
 
-            self.pre_trained_model = FastTextAddressSeq2SeqModel()
+            self.data_converter = data_padding
+
+            self.pre_trained_model = PretrainedFastTextSeq2SeqModel(device)
 
         elif model == "bpemb":
-            embeddings_model = bp_embeddings_model(lang="multi", vs=100000, dim=300)
-            self.vectorizer = BPEmbVectorizer(embeddings_model=embeddings_model, target_converter=target_converter)
+            self.vectorizer = BPEmbVectorizer(embeddings_model=BPEmbEmbeddingsModel(lang="multi", vs=100000, dim=300))
 
-            self.pre_trained_model = BPEmbAddressSeq2SeqModel()
+            self.data_converter = bpemb_data_padding
+
+            self.pre_trained_model = PretrainedBPEmbSeq2SeqModel(device)
+
         elif model == "fasttext-att":
             pass
         elif model == "bpemb-att":
             pass
         elif model == "best":
             pass
+        elif model == "lightest":
+            pass
+
+    def __call__(self, address_to_parse: Union[List[str], str]):
+        if isinstance(str, address_to_parse):
+            address_to_parse = list(address_to_parse)
+
+        # todo send to device
+        self.vectorizer(address_to_parse)  # input must be a list
+
+        # todo step pour convertir pour la "forward pass"
+        self.data_converter()
+
+        prediction = self.pre_trained_model(...)
+
+        # get max prob
+        # convert to tag

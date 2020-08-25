5c9f1a78ec305999c50784644aefa765d78637d0
davebulaval <david.beauchemin.5@ulaval.ca>
2020-08-25 15:42:00 -0400

    partial doc and typing for decoder/encoder

--- deepParse/model/decoder.py
+++ deepParse/model/decoder.py
@@ -4,7 +4,18 @@ from deepParse.tools import weight_init
 
 
 class Decoder(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, output_size):
+    """
+    Decoder module that use a LSTM to decode a previously encoded sequence and a linear layer to map
+    the decoded sequence tags.
+
+    Args:
+        input_size (int): The input size of the decoder.
+        hidden_size (int): The hidden size of the decoder.
+        num_layers (int): The number of layer to the decoder.
+        output_size (int): The output size of the decoder (i.e. the number of tags to predict on).
+    """
+
+    def __init__(self, input_size: int, hidden_size: int, num_layers: int, output_size: int):
         super().__init__()
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers=num_layers)
         self.lstm.apply(weight_init)
@@ -14,8 +25,14 @@ class Decoder(nn.Module):
 
         self.softmax = nn.LogSoftmax(dim=1)
 
-    def forward(self, input_, hidden):
-        output, _ = self.lstm(input_.float(), hidden)
+    def forward(self, to_predict, hidden): # todo validate input/output type
+        """
+
+        Return:
+            The prediction vector.
+
+        """
+        output, _ = self.lstm(to_predict.float(), hidden)
 
         output_prob = self.softmax(self.linear(output[0]))
 
--- deepParse/model/encoder.py
+++ deepParse/model/encoder.py
@@ -5,14 +5,26 @@ from deepParse.tools import weight_init
 
 
 class Encoder(nn.Module):
+    """
+    Encoder module that use a LSTM to encode a sequence.
+
+    Args:
+        input_size (int): The input size of the decoder.
+        hidden_size (int): The hidden size of the decoder.
+        num_layers (int): The number of layer to the decoder.
+    """
+
     def __init__(self, input_size, hidden_size, num_layers):
         super().__init__()
 
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers=num_layers, batch_first=True)
         self.lstm.apply(weight_init)
 
-    def forward(self, input_, lenghts_tensor):
-        packed_sequence = pack_padded_sequence(input_, lenghts_tensor, batch_first=True)
+    def forward(self, to_predict, lenghts_tensor):  # todo validate input/output type
+        """
+
+        """
+        packed_sequence = pack_padded_sequence(to_predict, lenghts_tensor, batch_first=True)
 
         _, hidden = self.lstm(packed_sequence, self.hidden)
 

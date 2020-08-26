5baedfa3bb6675327f13d853a5b9259b80483a9b
davebulaval <david.beauchemin.5@ulaval.ca>
2020-08-26 11:31:18 -0400

    forgot import in init when splitted pre_trained seq2seq

--- deepParse/model/__init__.py
+++ deepParse/model/__init__.py
@@ -2,4 +2,6 @@
 from .decoder import *
 from .embedding_network import *
 from .encoder import *
-from .seq2seq import *
+from .pre_trained_bpemb_seq2seq import *
+from .pre_trained_fasttext_seq2seq import *
+from .pre_trained_seq2seq import *

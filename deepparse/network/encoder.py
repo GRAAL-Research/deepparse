606b640f002a49f470ecb53db9e73695be3914e4
davebulaval <david.beauchemin.5@ulaval.ca>
2020-09-23 09:45:52 -0400

    bug-fix for new torch API

--- deepparse/network/encoder.py
+++ deepparse/network/encoder.py
@@ -35,7 +35,7 @@ class Encoder(nn.Module):
                 A tuple of the address components encoding.
         """
 
-        packed_sequence = pack_padded_sequence(to_predict, lengths_tensor, batch_first=True)
+        packed_sequence = pack_padded_sequence(to_predict, lengths_tensor, batch_first=True, enforce_sorted=False)
 
         _, hidden = self.lstm(packed_sequence)
 

130391d63343665eadfb21a64488f8a94c0aab4d
davebulaval <david.beauchemin.5@ulaval.ca>
2020-11-14 22:48:22 -0500

    path management error with args

--- fine_tuning_script.py
+++ fine_tuning_script.py
@@ -9,7 +9,7 @@ from deepparse.parser import AddressParser
 def main(args):
     address_parser = AddressParser(model=args.model_type, device=0)
 
-    train_container = PickleDatasetContainer(args.dataset_path)
+    train_container = PickleDatasetContainer(args.train_dataset_path)
 
     early_stopping = EarlyStopping(patience=10)
     lr_scheduler = ReduceLROnPlateau(patience=2)
@@ -17,7 +17,7 @@ def main(args):
     address_parser.retrain(train_container, 0.8, epochs=100, batch_size=1024, num_workers=6,
                            callbacks=[early_stopping, lr_scheduler], logging_path=f"./chekpoints/{args.model_type}")
 
-    test_container = PickleDatasetContainer(args.dataset_path)
+    test_container = PickleDatasetContainer(args.test_dataset_path)
 
     address_parser.test(test_container, batch_size=1024, num_workers=3, logging_path=f"./chekpoints/{args.model_type}")
 

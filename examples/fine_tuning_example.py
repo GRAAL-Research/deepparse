919f1e3d6ed76285e7e3db2c8e9b146e1c6fafb4
davebulaval <david.beauchemin.5@ulaval.ca>
2020-11-21 10:50:50 -0500

    error in file extension

--- examples/fine_tuning_example.py
+++ examples/fine_tuning_example.py
@@ -6,13 +6,13 @@ from deepparse.parser import AddressParser
 
 # First, let's download the train and test data from the public repository
 saving_dir = "./data/"
-file_extension = ".p"
+file_extension = "p"
 training_dataset_name = "sample_noisy_data"
 test_dataset_name = "test_sample_data"
 download_from_url(training_dataset_name, saving_dir, file_extension=file_extension)
 download_from_url(test_dataset_name, saving_dir, file_extension=file_extension)
 
-training_container = PickleDatasetContainer(saving_dir + training_dataset_name + file_extension)
+training_container = PickleDatasetContainer(saving_dir + training_dataset_name + "." + file_extension)
 
 address_parser = AddressParser(model="fasttext", device=0)
 
@@ -23,7 +23,7 @@ lr_scheduler = poutyne.StepLR(step_size=1, gamma=0.1)  # reduce LR by a factor o
 # the ckpt are saved in the default "./chekpoints" directory
 address_parser.retrain(training_container, 0.8, epochs=5, batch_size=8, num_workers=2, callbacks=[lr_scheduler])
 
-test_container = PickleDatasetContainer(saving_dir + test_dataset_name + ".p")
+test_container = PickleDatasetContainer(saving_dir + test_dataset_name + "." + file_extension)
 
 # Now let's test our fine tuned model
 address_parser.test(test_container, batch_size=256)

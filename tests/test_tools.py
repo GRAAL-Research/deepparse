6aa8b671d4db2cfbe24534428c1193e8b9716f60
davebulaval <david.beauchemin.5@ulaval.ca>
2020-12-09 15:54:04 -0500

    added test for handle chkpt

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,9 +1,13 @@
+# Since we use a patch to mock verify last we skip the unused argument error
+# pylint: disable=W0613
+
 import os
 from unittest import TestCase
+from unittest.mock import patch
 
 import requests
 
-from deepparse import download_from_url, latest_version
+from deepparse import download_from_url, latest_version, download_weights, handle_checkpoint, CACHE_PATH
 
 
 class ToolsTests(TestCase):
@@ -22,20 +26,20 @@ class ToolsTests(TestCase):
         file.close()
 
     def test_givenAFasttextLatestVersion_whenVerifyIfLastVersion_thenReturnTrue(self):
-        self.create_cache_version('fasttext', self.latest_fasttext_version)
-        self.assertTrue(latest_version('fasttext', self.fake_cache_path))
+        self.create_cache_version("fasttext", self.latest_fasttext_version)
+        self.assertTrue(latest_version("fasttext", self.fake_cache_path))
 
     def test_givenAFasttextNotTheLatestVersion_whenVerifyIfLastVersion_thenReturnFalse(self):
-        self.create_cache_version('fasttext', "not_the_last_version")
-        self.assertFalse(latest_version('fasttext', self.fake_cache_path))
+        self.create_cache_version("fasttext", "not_the_last_version")
+        self.assertFalse(latest_version("fasttext", self.fake_cache_path))
 
     def test_givenABPEmbLatestVersion_whenVerifyIfLastVersion_thenReturnTrue(self):
-        self.create_cache_version('bpemb', self.latest_bpemb_version)
-        self.assertTrue(latest_version('bpemb', self.fake_cache_path))
+        self.create_cache_version("bpemb", self.latest_bpemb_version)
+        self.assertTrue(latest_version("bpemb", self.fake_cache_path))
 
     def test_givenABPEmbNotTheLatestVersion_whenVerifyIfLastVersion_thenReturnFalse(self):
-        self.create_cache_version('bpemb', "not_the_last_version")
-        self.assertFalse(latest_version('bpemb', self.fake_cache_path))
+        self.create_cache_version("bpemb", "not_the_last_version")
+        self.assertFalse(latest_version("bpemb", self.fake_cache_path))
 
     def test_givenFasttextVersion_whenDownloadOk_thenDownloadIt(self):
         file_name = "fasttext"
@@ -62,3 +66,91 @@ class ToolsTests(TestCase):
 
         with self.assertRaises(requests.exceptions.HTTPError):
             download_from_url(wrong_file_name, self.a_saving_dir, self.a_file_extension)
+
+    def test_givenModelWeightsToDownload_whenDownloadOk_thenWeightsAreDownloaded(self):
+        with patch("deepparse.tools.download_from_url") as downloader:
+            download_weights(model="fasttext", saving_dir="./")
+
+            downloader.assert_any_call("fasttext", "./", "ckpt")
+            downloader.assert_any_call("fasttext", "./", "version")
+
+        with patch("deepparse.tools.download_from_url") as downloader:
+            download_weights(model="bpemb", saving_dir="./")
+
+            downloader.assert_any_call("bpemb", "./", "ckpt")
+            downloader.assert_any_call("bpemb", "./", "version")
+
+    def test_givenABestCheckpoint_whenHandleCheckpoint_thenReturnBest(self):
+        checkpoint = "best"
+
+        actual = handle_checkpoint(checkpoint)
+        expected = checkpoint
+
+        self.assertEqual(actual, expected)
+
+    def test_givenALastCheckpoint_whenHandleCheckpoint_thenReturnLast(self):
+        checkpoint = "last"
+
+        actual = handle_checkpoint(checkpoint)
+        expected = checkpoint
+
+        self.assertEqual(actual, expected)
+
+    def test_givenAIntCheckpoint_whenHandleCheckpoint_thenReturn1(self):
+        checkpoint = 1
+
+        actual = handle_checkpoint(checkpoint)
+        expected = checkpoint
+
+        self.assertEqual(actual, expected)
+
+    @patch("deepparse.tools.latest_version")
+    def test_givenAFasttextCheckpoint_whenHandleCheckpoint_thenReturnCachedFasttextPath(self, latest_version_check):
+        checkpoint = "fasttext"
+
+        actual = handle_checkpoint(checkpoint)
+        expected = os.path.join(CACHE_PATH, checkpoint + ".p")
+
+        self.assertEqual(actual, expected)
+
+    @patch("deepparse.tools.latest_version")
+    def test_givenABPEmbCheckpoint_whenHandleCheckpoint_thenReturnCachedBPEmbPath(self, latest_version_check):
+        checkpoint = "bpemb"
+
+        actual = handle_checkpoint(checkpoint)
+        expected = os.path.join(CACHE_PATH, checkpoint + ".p")
+
+        self.assertEqual(actual, expected)
+
+    def test_givenAStringCheckpoint_whenHandleCheckpoint_thenReturnSamePath(self):
+        pickle_checkpoint = "/a/path/to/a/model.p"
+
+        actual = handle_checkpoint(pickle_checkpoint)
+        expected = pickle_checkpoint
+
+        self.assertEqual(actual, expected)
+
+    def test_givenBadNamesCheckpoint_whenHandleCheckpoint_thenRaiseErrors(self):
+        with self.assertRaises(ValueError):
+            bad_best_checkpoint = "bests"
+            handle_checkpoint(bad_best_checkpoint)
+
+        with self.assertRaises(ValueError):
+            bad_last_checkpoint = "lasts"
+            handle_checkpoint(bad_last_checkpoint)
+
+        with self.assertRaises(ValueError):
+            string_int_bad_checkpoint = "1"
+            handle_checkpoint(string_int_bad_checkpoint)
+
+        with self.assertRaises(ValueError):
+            bad_fasttext_checkpoint = "fasttexts"
+            handle_checkpoint(bad_fasttext_checkpoint)
+
+        with self.assertRaises(ValueError):
+            bad_bpemb_checkpoint = "bpembds"
+            handle_checkpoint(bad_bpemb_checkpoint)
+
+        with self.assertRaises(ValueError):
+            bad_pickle_extension_checkpoint = "/a/path/to/a/model.pck"
+            handle_checkpoint(bad_pickle_extension_checkpoint)

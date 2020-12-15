5e9f6ff9bc9e98bb1aac9e8f740e031e058343db
davebulaval <david.beauchemin.5@ulaval.ca>
2020-12-15 11:02:20 -0500

    two directory and was not deleting file

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -11,10 +11,8 @@ from tests.tools import create_file
 class ToolsTests(TestCase):
 
     def setUp(self) -> None:
-        self.a_saving_dir = "./tmp"
-        self.a_file_extension = "version"
-
         self.fake_cache_path = "./"
+        self.a_file_extension = "version"
         self.latest_fasttext_version = "617a417a2f2b02654f7deb5b5cbc60ab2f6334ba"
         self.latest_bpemb_version = "6d01367745157066ea6e621ac087be828137711f"
 
@@ -47,28 +45,28 @@ class ToolsTests(TestCase):
     def test_givenFasttextVersion_whenDownloadOk_thenDownloadIt(self):
         file_name = "fasttext"
 
-        download_from_url(file_name, self.a_saving_dir, self.a_file_extension)
+        download_from_url(file_name, self.fake_cache_path, self.a_file_extension)
 
-        self.assertTrue(os.path.exists(os.path.join(self.a_saving_dir, f"{file_name}.{self.a_file_extension}")))
+        self.assertTrue(os.path.exists(os.path.join(self.fake_cache_path, f"{file_name}.{self.a_file_extension}")))
 
     def test_givenFasttextVersion_whenDownload404_thenHTTPError(self):
         wrong_file_name = "wrong_fasttext"
 
         with self.assertRaises(requests.exceptions.HTTPError):
-            download_from_url(wrong_file_name, self.a_saving_dir, self.a_file_extension)
+            download_from_url(wrong_file_name, self.fake_cache_path, self.a_file_extension)
 
     def test_givenBPEmbVersion_whenDownloadOk_thenDownloadIt(self):
         file_name = "bpemb"
 
-        download_from_url(file_name, self.a_saving_dir, self.a_file_extension)
+        download_from_url(file_name, self.fake_cache_path, self.a_file_extension)
 
-        self.assertTrue(os.path.exists(os.path.join(self.a_saving_dir, f"{file_name}.{self.a_file_extension}")))
+        self.assertTrue(os.path.exists(os.path.join(self.fake_cache_path, f"{file_name}.{self.a_file_extension}")))
 
     def test_givenBPEmbVersion_whenDownload404_thenHTTPError(self):
         wrong_file_name = "wrong_bpemb"
 
         with self.assertRaises(requests.exceptions.HTTPError):
-            download_from_url(wrong_file_name, self.a_saving_dir, self.a_file_extension)
+            download_from_url(wrong_file_name, self.fake_cache_path, self.a_file_extension)
 
     def test_givenModelWeightsToDownload_whenDownloadOk_thenWeightsAreDownloaded(self):
         with patch("deepparse.tools.download_from_url") as downloader:

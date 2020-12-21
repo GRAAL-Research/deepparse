24bc26680dd4b448d928412f2bae1c003103fd26
davebulaval <david.beauchemin.5@ulaval.ca>
2020-12-21 16:18:30 -0500

    added test for retrain weights integration test

--- tests/network/integration/test_integration_seq2seq_bpemb_model.py
+++ tests/network/integration/test_integration_seq2seq_bpemb_model.py
@@ -4,6 +4,7 @@
 
 from unittest import skipIf
 
+import os
 import torch
 
 from deepparse.network import BPEmbSeq2SeqModel
@@ -15,26 +16,51 @@ class BPEmbSeq2SeqIntegrationTest(Seq2SeqIntegrationTestCase):
 
     def setUp(self) -> None:
         # will load the weights if not local
-        self.pre_trained_seq2seq_model = BPEmbSeq2SeqModel(self.a_torch_device)
         self.encoder_input_setUp("bpemb")
         self.decomposition_lengths = [[1, 1, 1, 1, 1, 6], [1, 1, 1, 1, 1, 6]]
 
+        self.a_retrain_model = os.path.join(os.path.expanduser("~"), ".cache", "deepparse", "bpemb.ckpt")
+
     def test_whenForwardStep_thenStepIsOk(self):
+        self.seq2seq_model = BPEmbSeq2SeqModel(self.a_torch_device)
         # forward pass for two address: '['15 major st london ontario n5z1e1', '15 major st london ontario n5z1e1']'
         self.decoder_input_setUp()
 
-        predictions = self.pre_trained_seq2seq_model.forward(self.to_predict_tensor, self.decomposition_lengths,
-                                                             self.a_lengths_tensor)
+        predictions = self.seq2seq_model.forward(self.to_predict_tensor, self.decomposition_lengths,
+                                                 self.a_lengths_tensor)
 
         self.assert_output_is_valid_dim(predictions)
 
     def test_whenForwardStepWithTarget_thenStepIsOk(self):
+        self.seq2seq_model = BPEmbSeq2SeqModel(self.a_torch_device)
         # forward pass for two address: '['15 major st london ontario n5z1e1', '15 major st london ontario n5z1e1']'
         self.decoder_input_setUp()
 
-        predictions = self.pre_trained_seq2seq_model.forward(self.to_predict_tensor, self.decomposition_lengths,
-                                                             self.a_lengths_tensor, self.a_target_vector)
+        predictions = self.seq2seq_model.forward(self.to_predict_tensor, self.decomposition_lengths,
+                                                 self.a_lengths_tensor, self.a_target_vector)
 
         self.assert_output_is_valid_dim(predictions)
 
-    # todo test for loading pretrained
+    def test_retrainedModel_whenForwardStep_thenStepIsOk(self):
+        self.seq2seq_model = BPEmbSeq2SeqModel(self.a_torch_device,
+                                               self.verbose,
+                                               path_to_retrained_model=self.a_retrain_model)
+        # forward pass for two address: '['15 major st london ontario n5z1e1', '15 major st london ontario n5z1e1']'
+        self.decoder_input_setUp()
+
+        predictions = self.seq2seq_model.forward(self.to_predict_tensor, self.decomposition_lengths,
+                                                 self.a_lengths_tensor)
+
+        self.assert_output_is_valid_dim(predictions)
+
+    def test_retrainedModel_whenForwardStepWithTarget_thenStepIsOk(self):
+        self.seq2seq_model = BPEmbSeq2SeqModel(self.a_torch_device,
+                                               self.verbose,
+                                               path_to_retrained_model=self.a_retrain_model)
+        # forward pass for two address: '['15 major st london ontario n5z1e1', '15 major st london ontario n5z1e1']'
+        self.decoder_input_setUp()
+
+        predictions = self.seq2seq_model.forward(self.to_predict_tensor, self.decomposition_lengths,
+                                                 self.a_lengths_tensor, self.a_target_vector)
+
+        self.assert_output_is_valid_dim(predictions)
--- tests/network/integration/test_integration_seq2seq_fasttext_model.py
+++ tests/network/integration/test_integration_seq2seq_fasttext_model.py
@@ -4,6 +4,7 @@
 
 from unittest import skipIf
 
+import os
 import torch
 
 from deepparse.network import FastTextSeq2SeqModel
@@ -14,25 +15,48 @@ from ..integration.base import Seq2SeqIntegrationTestCase
 class FastTextSeq2SeqIntegrationTest(Seq2SeqIntegrationTestCase):
 
     def setUp(self) -> None:
+        super().setUp()
         # will load the weights if not local
-        self.pre_trained_seq2seq_model = FastTextSeq2SeqModel(self.a_torch_device)
         self.encoder_input_setUp("fasttext")
 
+        self.a_retrain_model = os.path.join(os.path.expanduser("~"), ".cache", "deepparse", "fasttext.ckpt")
+
     def test_whenForwardStep_thenStepIsOk(self):
+        self.seq2seq_model = FastTextSeq2SeqModel(self.a_torch_device)
         # forward pass for two address: '['15 major st london ontario n5z1e1', '15 major st london ontario n5z1e1']'
         self.decoder_input_setUp()
 
-        predictions = self.pre_trained_seq2seq_model.forward(self.to_predict_tensor, self.a_lengths_tensor)
+        predictions = self.seq2seq_model.forward(self.to_predict_tensor, self.a_lengths_tensor)
 
         self.assert_output_is_valid_dim(predictions)
 
     def test_whenForwardStepWithTarget_thenStepIsOk(self):
+        self.seq2seq_model = FastTextSeq2SeqModel(self.a_torch_device)
         # forward pass for two address: '['15 major st london ontario n5z1e1', '15 major st london ontario n5z1e1']'
         self.decoder_input_setUp()
 
-        predictions = self.pre_trained_seq2seq_model.forward(self.to_predict_tensor, self.a_lengths_tensor,
-                                                             self.a_target_vector)
+        predictions = self.seq2seq_model.forward(self.to_predict_tensor, self.a_lengths_tensor, self.a_target_vector)
 
         self.assert_output_is_valid_dim(predictions)
 
-    # todo test for loading pretrained
+    def test_retrainedModel_whenForwardStep_thenStepIsOk(self):
+        self.seq2seq_model = FastTextSeq2SeqModel(self.a_torch_device,
+                                                  self.verbose,
+                                                  path_to_retrained_model=self.a_retrain_model)
+        # forward pass for two address: '['15 major st london ontario n5z1e1', '15 major st london ontario n5z1e1']'
+        self.decoder_input_setUp()
+
+        predictions = self.seq2seq_model.forward(self.to_predict_tensor, self.a_lengths_tensor)
+
+        self.assert_output_is_valid_dim(predictions)
+
+    def test_retrainedModel_whenForwardStepWithTarget_thenStepIsOk(self):
+        self.seq2seq_model = FastTextSeq2SeqModel(self.a_torch_device,
+                                                  self.verbose,
+                                                  path_to_retrained_model=self.a_retrain_model)
+        # forward pass for two address: '['15 major st london ontario n5z1e1', '15 major st london ontario n5z1e1']'
+        self.decoder_input_setUp()
+
+        predictions = self.seq2seq_model.forward(self.to_predict_tensor, self.a_lengths_tensor, self.a_target_vector)
+
+        self.assert_output_is_valid_dim(predictions)

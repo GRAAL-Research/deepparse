47741744387d223a114d2ccfdbca3521977939c5
davebulaval <david.beauchemin.5@ulaval.ca>
2020-12-25 13:02:18 -0500

    added tests for model_evaluation tools

--- models_evaluation/tools.py
+++ models_evaluation/tools.py
@@ -23,34 +23,43 @@ def clean_up_name(country: str) -> str:
     return country
 
 
+# country that we trained on
 train_test_files = [
-    'br.p', 'us.p', 'kp.p', 'ru.p', 'de.p', 'fr.p', 'nl.p', 'ch.p', 'fi.p', 'es.p', 'cz.p', 'gb.p', 'mx.p', 'no.p',
-    'ca.p', 'it.p', 'au.p', 'dk.p', 'pl.p', 'at.p'
+    "br.p", "us.p", "kp.p", "ru.p", "de.p", "fr.p", "nl.p", "ch.p", "fi.p", "es.p", "cz.p", "gb.p", "mx.p", "no.p",
+    "ca.p", "it.p", "au.p", "dk.p", "pl.p", "at.p"
 ]
 
 
-# country that we trained on
 def train_country_file(file: str) -> bool:
+    """
+    Validate if a file is a training country (as reference of our article).
+    """
     return file in train_test_files
 
 
 # country that we did not train on
 other_test_files = [
-    'ie.p', 'rs.p', 'uz.p', 'ua.p', 'za.p', 'py.p', 'gr.p', 'dz.p', 'by.p', 'se.p', 'pt.p', 'hu.p', 'is.p', 'co.p',
-    'lv.p', 'my.p', 'ba.p', 'in.p', 're.p', 'hr.p', 'ee.p', 'nc.p', 'jp.p', 'nz.p', 'sg.p', 'ro.p', 'bd.p', 'sk.p',
-    'ar.p', 'kz.p', 've.p', 'id.p', 'bg.p', 'cy.p', 'bm.p', 'md.p', 'si.p', 'lt.p', 'ph.p', 'be.p', 'fo.p'
+    "ie.p", "rs.p", "uz.p", "ua.p", "za.p", "py.p", "gr.p", "dz.p", "by.p", "se.p", "pt.p", "hu.p", "is.p", "co.p",
+    "lv.p", "my.p", "ba.p", "in.p", "re.p", "hr.p", "ee.p", "nc.p", "jp.p", "nz.p", "sg.p", "ro.p", "bd.p", "sk.p",
+    "ar.p", "kz.p", "ve.p", "id.p", "bg.p", "cy.p", "bm.p", "md.p", "si.p", "lt.p", "ph.p", "be.p", "fo.p"
 ]
 
 
 def zero_shot_eval_country_file(file: str) -> bool:
+    """
+    Validate if a file is a zero shot country (as reference of our article).
+    """
     return file in other_test_files
 
 
 def test_on_country_data(address_parser: AddressParser, file: str, directory_path: str, args) -> tuple:
-    country = pycountry.countries.get(alpha_2=file.replace('.p', '').upper()).name
+    """
+    Compute the results over a country data.
+    """
+    country = pycountry.countries.get(alpha_2=file.replace(".p", "").upper()).name
     country = clean_up_name(country)
 
-    print(f'Testing on test files {country}')
+    print(f"Testing on test files {country}")
 
     test_file_path = os.path.join(directory_path, file)
     test_container = PickleDatasetContainer(test_file_path)

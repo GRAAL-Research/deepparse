0938bc52816aef114dbdf41fc15722420b1501f2
davebulaval <david.beauchemin.5@ulaval.ca>
2020-12-30 22:56:18 -0500

    added fine tuning tables

--- models_evaluation/generate_tables.py
+++ models_evaluation/generate_tables.py
@@ -1,9 +1,11 @@
 from models_evaluation.tools import make_table, make_table_rst
 
 make_table(data_type="training")
+make_table(data_type="training_fine_tuned")
 make_table(data_type="training_noisy")
 make_table(data_type="zero_shot")
 
 make_table_rst(data_type="training")
+make_table_rst(data_type="training_fine_tuned")
 make_table_rst(data_type="training_noisy")
 make_table_rst(data_type="zero_shot")

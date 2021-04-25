3018a3d6cd851da1f65ce2cb6e0b561670d36356
davebulaval <david.beauchemin.5@ulaval.ca>
2021-04-25 10:37:25 -0400

    added test for verbose print or now of downloading fasttext

--- tests/test_fasttext_tools.py
+++ tests/test_fasttext_tools.py
@@ -7,7 +7,7 @@ import os
 import sys
 import unittest
 from unittest import TestCase
-from unittest.mock import patch
+from unittest.mock import patch, mock_open
 
 from deepparse import download_fasttext_embeddings
 from deepparse.fasttext_tools import _print_progress
@@ -16,10 +16,14 @@ from tests.tools import create_file
 
 class ToolsTests(TestCase):
 
-    def setUp(self) -> None:
-        self.a_directory_path = "./"
-        self.a_fasttext_file_name_path = os.path.join(self.a_directory_path, "cc.fr.300.bin")
-        self.a_fasttext_gz_file_name_path = os.path.join(self.a_directory_path, "cc.fr.300.bin.gz")
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.a_directory_path = "./"
+        cls.a_fasttext_file_name_path = os.path.join(cls.a_directory_path, "cc.fr.300.bin")
+        cls.a_fasttext_gz_file_name_path = os.path.join(cls.a_directory_path, "cc.fr.300.bin.gz")
+
+        # the payload is a first "chunk" a, a second chunk "b" and a empty chunk "" to end the loop
+        cls.a_response_payload = ["a", "b", ""]
 
     def tearDown(self) -> None:
         if os.path.exists(self.a_fasttext_file_name_path):
@@ -76,6 +80,56 @@ class ToolsTests(TestCase):
                               self.test_out.getvalue().strip())
 
         self.assertStdoutContains(["[", ">", "=", "]"])
+
+    @patch("builtins.open", new_callable=mock_open, read_data="a sentence")
+    @patch("deepparse.fasttext_tools.urlopen")
+    @patch("deepparse.fasttext_tools.gzip")
+    @patch("deepparse.fasttext_tools.os.rename")
+    @patch("deepparse.fasttext_tools.shutil")
+    @patch("deepparse.fasttext_tools.os.remove")
+    def test_givenADownloadFasttext_whenPrintProgressSetToVerbose_thenDontPrint(self, os_remove_mock, shutil_mock,
+                                                                                os_rename_mock, g_zip_mock,
+                                                                                urlopen_mock, open_mock):
+        # pylint: disable=too-many-arguments
+        urlopen_mock().read.side_effect = self.a_response_payload
+        self._capture_output()
+        with urlopen_mock:
+            _ = download_fasttext_embeddings(self.a_directory_path, verbose=False)
+
+        expected = ""
+        actual = self.test_out.getvalue().strip()
+
+        self.assertEqual(expected, actual)
+
+    @patch("builtins.open", new_callable=mock_open, read_data="a sentence")
+    @patch("deepparse.fasttext_tools.urlopen")
+    @patch("deepparse.fasttext_tools.gzip")
+    @patch("deepparse.fasttext_tools.os.rename")
+    @patch("deepparse.fasttext_tools.shutil")
+    @patch("deepparse.fasttext_tools.os.remove")
+    def test_givenADownloadFasttext_whenPrintProgressSetToVerbose_thenPrint(self, os_remove_mock, shutil_mock,
+                                                                            os_rename_mock, g_zip_mock, urlopen_mock,
+                                                                            open_mock):
+        # pylint: disable=too-many-arguments
+        urlopen_mock().read.side_effect = self.a_response_payload
+        urlopen_mock().getheader.return_value = "2"
+        self._capture_output()
+        with urlopen_mock:
+            _ = download_fasttext_embeddings(self.a_directory_path, verbose=True)
+        actual = self.test_out.getvalue().strip()
+
+        expected = "The fastText pre-trained word embeddings will be downloaded (6.8 GO), " \
+                   "this process will take several minutes."
+        self.assertIn(expected, actual)
+
+        expected = "Downloading https://dl.fbaipublicfiles.com/fasttext/vectors-crawl/cc.fr.300.bin.gz"
+        self.assertIn(expected, actual)
+
+        expected = "(50.00%) [=========================>                         ]"
+        self.assertIn(expected, actual)
+
+        expected = "(100.00%) [==================================================>]"
+        self.assertIn(expected, actual)
 
 
 if __name__ == "__main__":

2b451f15aa4461a8f88f0c0f1fd92d076196d035
davebulaval <david.beauchemin.5@ulaval.ca>
2021-04-26 17:09:07 -0400

    reworded the models_evaluation workflow

--- models_evaluation/evaluate_model.py
+++ models_evaluation/evaluate_model.py
@@ -5,11 +5,12 @@ import json
 import os
 
 from deepparse.parser import AddressParser
-from models_evaluation.tools import train_country_file, zero_shot_eval_country_file, test_on_country_data, bool_parse
+from models_evaluation.tools import train_country_file, zero_shot_eval_country_file, test_on_country_data
 
 
 def main(args):
-    saving_dir = os.path.join(".", "models_evaluation", "results")
+    results_type = args.results_type
+    saving_dir = os.path.join(".", "models_evaluation", "results", results_type)
     os.makedirs(saving_dir, exist_ok=True)
 
     address_parser = AddressParser(model_type=args.model_type, device=0)
@@ -28,21 +29,17 @@ def main(args):
         else:
             print(f"Error with the identification of test file type {test_file}.")
 
-    if args.fine_tuning:
-        training_base_string = "training_fine_tuned_test_results"
-        training_incomplete_base_string = "training_incomplete_fine_tuned_test_results"
-        zero_shot_base_string = "zero_shot_fine_tuned_test_results"
-    else:
-        training_base_string = "training_test_results"
-        training_incomplete_base_string = "training_incomplete_test_results"
-        zero_shot_base_string = "zero_shot_test_results"
+    training_base_string = "training_test_results"
+    training_incomplete_base_string = "training_incomplete_test_results"
+    zero_shot_base_string = "zero_shot_test_results"
 
-    json.dump(training_test_results,
-              open(os.path.join(saving_dir, f"{training_base_string}_{args.model_type}.json"), "w", encoding="utf-8"),
-              ensure_ascii=False)
-    json.dump(zero_shot_test_results,
-              open(os.path.join(saving_dir, f"{zero_shot_base_string}_{args.model_type}.json"), "w", encoding="utf-8"),
-              ensure_ascii=False)
+    with open(os.path.join(saving_dir, f"{training_base_string}_{args.model_type}.json"), "w",
+              encoding="utf-8") as file:
+        json.dump(training_test_results, file, ensure_ascii=False)
+
+    with open(os.path.join(saving_dir, f"{zero_shot_base_string}_{args.model_type}.json"), "w",
+              encoding="utf-8") as file:
+        json.dump(zero_shot_test_results, file, ensure_ascii=False)
 
     incomplete_test_directory = args.incomplete_test_directory
     incomplete_test_files = os.listdir(incomplete_test_directory)
@@ -56,11 +53,10 @@ def main(args):
         else:
             print(f"Error with the identification of test file type {incomplete_test_file}.")
 
-    json.dump(incomplete_training_test_results,
-              open(os.path.join(saving_dir, f"{training_incomplete_base_string}_{args.model_type}.json"),
-                   "w",
-                   encoding="utf-8"),
-              ensure_ascii=False)
+    with open(os.path.join(saving_dir, f"{training_incomplete_base_string}_{args.model_type}.json"),
+              "w",
+              encoding="utf-8") as file:
+        json.dump(incomplete_training_test_results, file, ensure_ascii=False)
 
 
 if __name__ == "__main__":
@@ -70,12 +66,11 @@ if __name__ == "__main__":
     parser.add_argument("test_directory", type=str, help="Path to the test directory.")
     parser.add_argument("incomplete_test_directory", type=str, help="Path the to incomplete test directory.")
     parser.add_argument("model_path", type=str, help="Path to the model to evaluate on.")
-    parser.add_argument("--fine_tuning",
-                        type=bool_parse,
-                        help="Either or not the model to "
-                        "evaluation is a fine tuning one. This will only better "
-                        "create naming of results files.")
     parser.add_argument("--batch_size", type=int, default=2048, help="Batch size of the data to evaluate on.")
+    parser.add_argument("--results_type",
+                        type=str,
+                        default="actual",
+                        help="Either or not the evaluation is for new models.")
     args_parser = parser.parse_args()
 
     main(args_parser)

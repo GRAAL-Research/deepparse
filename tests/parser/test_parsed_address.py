a927baca59de54cf6824c3f3c1b622d383579c69
davebulaval <david.beauchemin.5@ulaval.ca>
2021-04-27 14:01:17 -0400

    added test for parsed address

--- tests/parser/test_parsed_address.py
+++ tests/parser/test_parsed_address.py
@@ -9,9 +9,16 @@ class ParsedAddressTest(CaptureOutputTestCase):
     @classmethod
     def setUpClass(cls):
         cls.a_address_str = "3 test road"
+        cls.a_complete_address_str = "3 test road unit west city province postal_code delivery"
         cls.a_parsed_address = [("3", "StreetNumber"), ("test", "StreetName"), ("road", "StreetName")]
+        cls.a_complete_parsed_address = [("3", "StreetNumber"), ("test", "StreetName"), ("road", "StreetName"),
+                                         ("unit", "Unit"), ("west", "Orientation"), ("city", "Municipality"),
+                                         ("province", "Province"), ("postal_code", "PostalCode"),
+                                         ("delivery", "GeneralDelivery")]
+
         cls.a_address_repr = "ParsedAddress<street_number='3', street_name='test road'>"
         cls.a_address = {cls.a_address_str: cls.a_parsed_address}
+        cls.a_complete_address = {cls.a_complete_address_str: cls.a_complete_parsed_address}
         cls.a_existing_tag = "3"
 
     def setUp(self):
@@ -26,6 +33,12 @@ class ParsedAddressTest(CaptureOutputTestCase):
         parsed_address = self.parsed_address.address_parsed_components
 
         self.assertEqual(parsed_address, self.a_parsed_address)
+
+    def test_whenInstantiatedWithCompleteAddress_thenShouldReturnCorrectCompleteParsedAddress(self):
+        self.parsed_address = ParsedAddress(self.a_complete_address)
+        parsed_address = self.parsed_address.address_parsed_components
+
+        self.assertEqual(parsed_address, self.a_complete_parsed_address)
 
     def test_whenInstantiatedWithAddress_thenShouldReturnCorrectTagIfExists(self):
         street_number = self.parsed_address.street_number

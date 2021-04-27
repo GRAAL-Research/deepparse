90dd72771967626114085cc8fad1b02de7411b07
davebulaval <david.beauchemin.5@ulaval.ca>
2021-04-27 11:29:52 -0400

    improved testing over embeddings model

---
+++ tests/embeddings_models/test_embeddings_model.py
@@ -0,0 +1,48 @@
+import io
+import sys
+import unittest
+from unittest import TestCase
+
+from deepparse.embeddings_models import EmbeddingsModel
+
+
+class CallAbstractedEmbeddingsModel(EmbeddingsModel):
+    def __call__(self, *args, **kwargs):
+        pass
+
+
+class EmbeddingsModelInterfaceTest(TestCase):
+    def _capture_output(self):
+        self.test_out = io.StringIO()
+        self.original_output = sys.stdout
+        sys.stdout = self.test_out
+
+    def test_whenInstantiated_thenInitProperly(self):
+        embeddings_model = CallAbstractedEmbeddingsModel(verbose=False)
+
+        self.assertIsNone(embeddings_model.model)
+
+    def test_whenInstantiatedVerbose_thenVerbose(self):
+        self._capture_output()
+        _ = CallAbstractedEmbeddingsModel(verbose=True)
+
+        actual = self.test_out.getvalue().strip()
+        expected = "Loading the embeddings model"
+        self.assertEqual(actual, expected)
+
+    def test_whenInstantiatedNotVerbose_thenNoVerbose(self):
+        self._capture_output()
+        _ = CallAbstractedEmbeddingsModel(verbose=False)
+
+        actual = self.test_out.getvalue().strip()
+        expected = ""
+        self.assertEqual(actual, expected)
+
+    def test_whenInstantiated_thenInitWithoutDim(self):
+        embeddings_model = CallAbstractedEmbeddingsModel(verbose=False)
+        with self.assertRaises(AttributeError):
+            _ = embeddings_model.dim
+
+
+if __name__ == "__main__":
+    unittest.main()

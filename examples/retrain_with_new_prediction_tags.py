e087fde39d854183c1bf3c0770a26563412d33e9
davebulaval <david.beauchemin.5@ulaval.ca>
2021-05-31 15:45:36 -0400

    fixed example retrain

--- examples/retrain_with_new_prediction_tags.py
+++ examples/retrain_with_new_prediction_tags.py
@@ -1,6 +1,6 @@
 import os
+
 import poutyne
-import torch
 
 from deepparse import download_from_url
 from deepparse.dataset_container import PickleDatasetContainer
@@ -19,7 +19,8 @@ training_container = PickleDatasetContainer(os.path.join(saving_dir, training_da
 test_container = PickleDatasetContainer(os.path.join(saving_dir, test_dataset_name + "." + file_extension))
 
 # We will retrain the fasttext version of our pretrained model.
-address_parser = AddressParser(model_type="fasttext", device=0)
+model = "fasttext"
+address_parser = AddressParser(model_type=model, device=0)
 
 # Now let's retrain for 5 epochs using a batch size of 8 since the data is really small for the example.
 # Let's start with the default learning rate of 0.01 and use a learning rate scheduler to lower the learning rate
@@ -32,15 +33,17 @@ tag_dictionary = {"ATag": 0, "AnotherTag": 1, "EOS": 2}
 # The path to save our checkpoints
 logging_path = "./checkpoints"
 
-# The checkpoints (ckpt) are saved in the default "./checkpoints" directory.
-# address_parser.retrain(training_container, 0.8, epochs=5, batch_size=8, num_workers=2, callbacks=[lr_scheduler],
-#                        prediction_tags=tag_dictionary, logging_path=logging_path)
+address_parser.retrain(training_container,
+                       0.8,
+                       epochs=5,
+                       batch_size=8,
+                       num_workers=2,
+                       callbacks=[lr_scheduler],
+                       prediction_tags=tag_dictionary,
+                       logging_path=logging_path)
 
 # The retrained model file name are formatted as retrained_{model_type}_address_parser.ckpt
-model_path = os.path.join(logging_path, "retrained_fasttext_address_parser.ckpt")
+model_path = os.path.join(logging_path, f"retrained_{model}_address_parser.ckpt")
+
 # Now let's test our fine tuned model using the best checkpoint (default parameter).
-# address_parser.test(test_container, batch_size=256, model_path=model_path)
-
-data = torch.load(os.path.join(logging_path, "retrained_fasttext_address_parser.ckpt"))
-
-print("a")
+address_parser.test(test_container, batch_size=256, model_path=model_path)

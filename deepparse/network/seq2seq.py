037ef74e13c84c525dbe6b2a8066ad9887be554e
davebulaval <david.beauchemin.5@ulaval.ca>
2021-06-09 15:03:53 -0400

    fixed long len error pylint

--- deepparse/network/seq2seq.py
+++ deepparse/network/seq2seq.py
@@ -15,8 +15,8 @@ from ..tools import download_weights, latest_version
 
 class Seq2SeqModel(ABC, nn.Module):
     """
-    Abstract class for Seq2Seq network. By default, the network uses the config as designed in our article for the encoder
-    and decoder:
+    Abstract class for Seq2Seq network. By default, the network uses the config as designed in our article for the
+    encoder and decoder:
 
         - Encoder: ``input_size = 300``, ``hidden_size = 1024`` and ``num_layers = 1``
         - Decoder: ``input_size = 1``, ``hidden_size = 1024``, ``num_layers = 1`` and ``output_size = 9``

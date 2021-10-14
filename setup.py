bbc7b1402c07951fb1fac439fab8cc617e1e11e6
David Beauchemin <david.beauchemin.5@ulaval.ca>
2021-10-14 11:38:58 -0400

    added python3.9 and fixed pylint (#102)
    * added python3.9 and fixed pylint
    
    * added windows 3.9 also

--- setup.py
+++ setup.py
@@ -60,6 +60,7 @@ def main():
             "Programming Language :: Python :: 3",
             "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
             "Topic :: Software Development :: Libraries",
             "Topic :: Software Development :: Libraries :: Python Modules"
         ],

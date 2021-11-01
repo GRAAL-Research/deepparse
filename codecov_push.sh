6322a789fd9808a9f122c2c2f98e1c45cde334a8
davebulaval <david.beauchemin.5@ulaval.ca>
2021-11-01 10:53:49 -0400

    improved manual codecov push

--- codecov_push.sh
+++ codecov_push.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 pip install pytest-cov
+pip install -Ur tests/requirements.txt
+pip install -Ur requirements.txt
 pytest --cov=./deepparse --cov-report=xml --cov-config=.coveragerc
 ./codecov -f coverage.xml -n unittest-integrationtest -t $CODECOVKEYDEEPPARSE

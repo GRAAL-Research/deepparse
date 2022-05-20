e5b919e8b4b7b26cc645a3f663e73fd544d65253
davebulaval <david.beauchemin.5@ulaval.ca>
2022-05-20 14:36:40 -0400

    fix model_type_naming for attention model

--- deepparse/parser/tools.py
+++ deepparse/parser/tools.py
@@ -95,7 +95,7 @@ def handle_model_name(model_type: str, attention_mechanism: bool) -> Tuple[str, 
         )
 
     if attention_mechanism:
-        model_type += "-attention"
+        model_type += "_attention"
         formatted_name += "Attention"
     return model_type, formatted_name
 

a4922ac24a5bb025bbcef8ba1ab5a7dc74be6512
davebulaval <david.beauchemin.5@ulaval.ca>
2022-07-06 12:53:28 -0400

    improve codecov script

--- codecov_push.sh
+++ codecov_push.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 
-pip install pytest-cov
+# We do test on Python 3.10
+
+# Create a new Python env for Deepparse tests and activate it
+conda create --name deepparse_pytest_3_10 python=3.10 -y --force
+conda activate deepparse_pytest_3_10
+
+# Install dependencies
 pip install -Ur tests/requirements.txt
 pip install -Ur requirements.txt
-pytest --cov=./deepparse --cov-report=xml --cov-config=.coveragerc
+
+# Run pytest from conda env
+conda run -n deepparse_pytest_3_10 pytest --cov ./deepparse --cov-report html --cov-report xml --cov-config=.coveragerc
+
+# Push the coverage file
 ./codecov -f coverage.xml -n unittest-integrationtest -t $CODECOVKEYDEEPPARSE
+
+# close conda env
+conda deactivate

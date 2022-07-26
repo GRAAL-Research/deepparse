79b5513c76d7c9cb2c4a7d482e60c0e0c769d103
davebulaval <david.beauchemin.5@ulaval.ca>
2022-07-26 11:46:31 -0400

    bump pylint and add django for codacy

---
+++ settings.py
@@ -0,0 +1,8 @@
+# Django settings for django_frontend project.
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'en-us'

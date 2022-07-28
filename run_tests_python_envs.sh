1dce60b9dd0c76b9143176852e0d225595fe9dd3
davebulaval <david.beauchemin.5@ulaval.ca>
2022-07-28 10:27:05 -0400

    improve run all tests script

--- run_tests_python_envs.sh
+++ run_tests_python_envs.sh
@@ -20,8 +20,15 @@ pip install -Ur ../requirements.txt
 echo "*****Running test in Conda Python version 3.7*****"
 conda run -n deepparse_pytest_3_7 --live-stream pytest --cov ../deepparse --cov-report html:html_report_3_7 --cov-report xml:export_xml_report_3_7.xml --cov-config=.coveragerc ../tests
 
+if [ $? -eq 0 ]; then
+  python3_7_tests_res=1
+fi
+
 # close conda env
 conda deactivate
+
+# Cleanup the conda env
+conda env remove -n deepparse_pytest_3_7
 
 # Create a new Python env 3.8
 conda create --name deepparse_pytest_3_8 python=3.8 -y --force
@@ -35,8 +42,15 @@ pip install -Ur ../requirements.txt
 echo "*****Running test in Conda Python version 3.8*****"
 conda run -n deepparse_pytest_3_8 --live-stream pytest --cov ../deepparse --cov-report html:html_report_3_8 --cov-report xml:export_xml_report_3_8.xml --cov-config=.coveragerc ../tests
 
+if [ $? -eq 0 ]; then
+  python3_8_tests_res=1
+fi
+
 # close conda env
 conda deactivate
+
+# Cleanup the conda env
+conda env remove -n deepparse_pytest_3_8
 
 # Create a new Python env 3.9
 conda create --name deepparse_pytest_3_9 python=3.9 -y --force
@@ -50,8 +64,15 @@ pip install -Ur ../requirements.txt
 echo "*****Running test in Conda Python version 3.9*****"
 conda run -n deepparse_pytest_3_9 --live-stream pytest --cov ../deepparse --cov-report html:html_report_3_9 --cov-report xml:export_xml_report_3_9.xml --cov-config=.coveragerc ../tests
 
+if [ $? -eq 0 ]; then
+  python3_9_tests_res=1
+fi
+
 # close conda env
 conda deactivate
+
+# Cleanup the conda env
+conda env remove -n deepparse_pytest_3_9
 
 # Create a new Python env 3.10
 conda create --name deepparse_pytest_3_10 python=3.10 -y --force
@@ -65,5 +86,39 @@ pip install -Ur ../requirements.txt
 echo "*****Running test in Conda Python version 3.10*****"
 conda run -n deepparse_pytest_3_10 --live-stream pytest --cov ../deepparse --cov-report html:html_report_3_10 --cov-report xml:export_xml_report_3_10.xml --cov-config=.coveragerc ../tests
 
+if [ $? -eq 0 ]; then
+  python3_10_tests_res=1
+fi
+
 # close conda env
 conda deactivate
+
+# Cleanup the conda env
+conda env remove -n deepparse_pytest_3_10
+
+# All tests env print
+echo "*****The results of the tests are:"
+
+if [ $python3_7_tests_res -eq 1 ]; then
+  echo "Success for Python 3.7"
+else
+  echo "Fail for Python 3.7"
+fi
+
+if [ $python3_8_tests_res -eq 1 ]; then
+  echo "Success for Python 3.8"
+else
+  echo "Fail for Python 3.8"
+fi
+
+if [ $python3_9_tests_res -eq 1 ]; then
+  echo "Success for Python 3.9"
+else
+  echo "Fail for Python 3.9"
+fi
+
+if [ $python3_10_tests_res -eq 1 ]; then
+  echo "Success for Python 3.10"
+else
+  echo "Fail for Python 3.10"
+fi

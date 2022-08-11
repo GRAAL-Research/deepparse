a936803240bde3b272a3520819e522f98fb118fd
davebulaval <david.beauchemin.5@ulaval.ca>
2022-08-10 21:21:09 -0400

    remove fn download_from_url

--- deepparse/tools.py
+++ deepparse/tools.py
@@ -82,15 +82,6 @@ def latest_version(model: str, cache_path: str, verbose: bool) -> bool:
     return is_latest_version
 
 
-def download_from_url(file_name: str, saving_dir: str, file_extension: str) -> None:  # pragma: no cover
-    warnings.warn(
-        "download_from_url is deprecated; use download_from_public_repository to download files from "
-        "our public repository. The function will be removed in the next major release.",
-        DeprecationWarning,
-    )
-    download_from_public_repository(file_name=file_name, saving_dir=saving_dir, file_extension=file_extension)
-
-
 def download_from_public_repository(file_name: str, saving_dir: str, file_extension: str) -> None:
     """
     Simple function to download the content of a file from Deepparse public repository.

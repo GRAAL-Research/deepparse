d6dcf1fa31045e0f1127541d8ced132c4a0d4065
Marouane Yassine <marouane.yassine.1@ulaval.ca>
2022-10-26 16:12:30 -0400

    tweaked process_for_training method

--- deepparse/converter/data_processor.py
+++ deepparse/converter/data_processor.py
@@ -24,10 +24,12 @@ class DataProcessor:
         # TODO: add teacher forcing
         input_sequence = []
         target_sequence = []
-        # TODO: use zip to seperate the addresses and targets
-        input_sequence.extend(self.vectorizer([address[0] for address in addresses_and_targets]))
 
-        for _, target_list in addresses_and_targets:
+        addresses, targets = zip(*addresses_and_targets)
+
+        input_sequence.extend(self.vectorizer(list(addresses)))
+
+        for target_list in targets:
             target_tmp = [self.tags_converter(target) for target in target_list]
             target_tmp.append(self.tags_converter("EOS"))  # to append the End Of Sequence token
             target_sequence.append(target_tmp)

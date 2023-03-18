1894b31b679565b9605b53f3be457984596d6710
davebulaval <david.beauchemin.5@ulaval.ca>
2023-03-18 14:41:23 -0400

    add python 3.11

--- codecov_push.sh
+++ codecov_push.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# We do test on Python 3.10
+# We do test on Python 3.11 (latest Python version)
 
 # Create a new Python env for Deepparse tests and activate it
-conda create --name deepparse_pytest_3_10 python=3.10 -y --force
-conda activate deepparse_pytest_3_10
+conda create --name deepparse_pytest_3_11 python=3.11 -y --force
+conda activate deepparse_pytest_3_11
 
 # Install dependencies
 pip install -Ur tests/requirements.txt
@@ -13,7 +13,7 @@ pip install -Ur requirements.txt
 # Run pytest from conda env
 echo "*****Running test in Conda*****"
 # --live-stream is to remove the Conda capture of the stream
-conda run -n deepparse_pytest_3_10 --live-stream pytest --cov ./deepparse --cov-report html --cov-report xml --cov-config=.coveragerc
+conda run -n deepparse_pytest_3_11 --live-stream pytest --cov ./deepparse --cov-report html --cov-report xml --cov-config=.coveragerc
 
 # Push the coverage file
 ./codecov -f coverage.xml -n unittest-integrationtest -t $CODECOVKEYDEEPPARSE
@@ -21,4 +21,4 @@ conda run -n deepparse_pytest_3_10 --live-stream pytest --cov ./deepparse --cov-
 # close conda env
 conda deactivate
 
-conda env remove -n deepparse_pytest_3_10
+conda env remove -n deepparse_pytest_3_11
--- run_tests_python_envs.sh
+++ run_tests_python_envs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# We test on Deepparse supported Python versions, namely, 3.7, 3.8, 3.9 and 3.10
-echo "*****Starting of testing Deepparse on Python version 3.7, 3.8, 3.9, 3.10*****"
+# We test on Deepparse supported Python versions, namely, 3.7, 3.8, 3.9, 3.10 and 3.11
+echo "*****Starting of testing Deepparse on Python version 3.7, 3.8, 3.9, 3.10, 3.11*****"
 
 # We export the reports into a directory. But to do so, we need to move into that directory
 # and run pytest from there
@@ -94,7 +94,29 @@ fi
 conda deactivate
 
 # Cleanup the conda env
-conda env remove -n deepparse_pytest_3_10
+conda env remove -n deepparse_pytest_3_11
+
+# Create a new Python env 3.11
+conda create --name deepparse_pytest_3_11 python=3.11 -y --force
+conda activate deepparse_pytest_3_11
+
+# Install dependencies
+pip install -Ur ../tests/requirements.txt
+pip install -Ur ../requirements.txt
+
+# Run pytest from conda env
+echo "*****Running test in Conda Python version 3.11*****"
+conda run -n deepparse_pytest_3_11 --live-stream pytest --cov ../deepparse --cov-report html:html_report_3_11 --cov-report xml:export_xml_report_3_11.xml --cov-config=.coveragerc ../tests
+
+if [ $? -eq 0 ]; then
+  python3_11_tests_res=1
+fi
+
+# close conda env
+conda deactivate
+
+# Cleanup the conda env
+conda env remove -n deepparse_pytest_3_11
 
 # All tests env print
 echo "*****The results of the tests are:"
@@ -126,6 +148,13 @@ if [ $python3_10_tests_res -eq 1 ]; then
 else
   return_status=1
   echo "Fail for Python 3.10"
+fi
+
+if [ $python3_11_tests_res -eq 1 ]; then
+  echo "Success for Python 3.11"
+else
+  return_status=1
+  echo "Fail for Python 3.11"
 fi
 
 if [ $return_status -eq 1 ]; then

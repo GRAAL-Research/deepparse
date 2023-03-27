817c8ea5927bbb137d6dc26d5777fe988e7e02d5
David Beauchemin <david.beauchemin.5@ulaval.ca>
2023-03-27 12:41:48 -0400

    drop python 3.7 (#187)

--- run_tests_python_envs.sh
+++ run_tests_python_envs.sh
@@ -2,35 +2,13 @@
 # To run using ZSH: zsh -i ./codecov_push.sh
 # To run using bash: bash -i ./codecov_push.sh
 
-# We test on Deepparse supported Python versions, namely, 3.7, 3.8, 3.9, 3.10 and 3.11
-echo "*****Starting of testing Deepparse on Python version 3.7, 3.8, 3.9, 3.10, 3.11*****"
+# We test on Deepparse supported Python versions, namely, 3.8, 3.9, 3.10 and 3.11
+echo "*****Starting of testing Deepparse on Python version 3.8, 3.9, 3.10, 3.11*****"
 
 # We export the reports into a directory. But to do so, we need to move into that directory
 # and run pytest from there
 mkdir -p export_html_reports
 cd ./export_html_reports
-
-# Create a new Python env 3.7
-conda create --name deepparse_pytest_3_7 python=3.7 -y --force
-conda activate deepparse_pytest_3_7
-
-# Install dependencies
-pip install -Ur ../tests/requirements.txt
-pip install -Ur ../requirements.txt
-
-# Run pytest from conda env
-echo "*****Running test in Conda Python version 3.7*****"
-conda run -n deepparse_pytest_3_7 --live-stream pytest --cov ../deepparse --cov-report html:html_report_3_7 --cov-report xml:export_xml_report_3_7.xml --cov-config=.coveragerc ../tests
-
-if [ $? -eq 0 ]; then
-  python3_7_tests_res=1
-fi
-
-# close conda env
-conda deactivate
-
-# Cleanup the conda env
-conda env remove -n deepparse_pytest_3_7
 
 # Create a new Python env 3.8
 conda create --name deepparse_pytest_3_8 python=3.8 -y --force
@@ -123,13 +101,6 @@ conda env remove -n deepparse_pytest_3_11
 # All tests env print
 echo "*****The results of the tests are:"
 return_status=0
-
-if [ $python3_7_tests_res -eq 1 ]; then
-  echo "Success for Python 3.7"
-else
-  return_status=1
-  echo "Fail for Python 3.7"
-fi
 
 if [ $python3_8_tests_res -eq 1 ]; then
   echo "Success for Python 3.8"

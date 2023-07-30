d838d036394730ab57d1c6f1cf457732245b4273
davebulaval <david.beauchemin.5@ulaval.ca>
2023-07-30 14:59:40 -0400

    libpostal test with memory profiler

--- models_evaluation/speed_test_evaluation_libpostal.py
+++ models_evaluation/speed_test_evaluation_libpostal.py
@@ -1,5 +1,6 @@
 import os.path
 
+from memory_profiler import profile
 from postal.parser import parse_address
 
 from deepparse import download_from_public_repository
@@ -14,23 +15,31 @@ addresses, tags = zip(*address_container)
 speed_test_directory = "results/speed_test_results"
 os.makedirs(speed_test_directory, exist_ok=True)
 
-with open(
-    os.path.join(
-        speed_test_directory,
-        f"speed_test_results_with_libpostal.txt",
-    ),
-    "w",
-) as file:
+
+@profile
+def process_fn():
+    for address in addresses:
+        parse_address(address)
+
+
+if __name__ == '__main__':
     timer = Timer()
     with timer:
-        for address in addresses:
-            parse_address(address)
-    print(
-        "Temps moyen pour porcess avec Libpostal :",
-        timer.elapsed_time / len(addresses),
-        file=file,
-    )
-    print(
-        "Temps moyen pour porcess avec Libpostal :",
-        timer.elapsed_time / len(addresses),
-    )
+        process_fn()
+
+    with open(
+        os.path.join(
+            speed_test_directory,
+            f"speed_test_results_with_libpostal.txt",
+        ),
+        "w",
+    ) as file:
+        print(
+            "Temps moyen pour porcess avec Libpostal :",
+            timer.elapsed_time / len(addresses),
+            file=file,
+        )
+        print(
+            "Temps moyen pour porcess avec Libpostal :",
+            timer.elapsed_time / len(addresses),
+        )

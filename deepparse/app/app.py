93c55d83d94283945fc996096edf6af2d3b6e67a
Olivier Belhumeur <belhumeurolivier@gmail.com>
2023-10-05 10:20:56 -0400

    removed f string

--- deepparse/app/app.py
+++ deepparse/app/app.py
@@ -7,7 +7,7 @@ try:
     from fastapi.staticfiles import StaticFiles
     import uvicorn
     import os
-    import warnings
+    import logging
 
 except ModuleNotFoundError as e:
     raise ModuleNotFoundError("Ensure you installed the extra packages using: 'pip install deepparse[app]'") from e
@@ -23,7 +23,7 @@ html_build_path = "docs/_build/html"
 if os.path.exists(html_build_path):
     app.mount("/", StaticFiles(directory=html_build_path, html=True), name="static")
 else:
-    warnings.warn(f"Unable to mount static files, probably because the docs are not built yet: {html_build_path}")
+    logging.warning("Unable to mount static files, probably because the docs are not built yet: %s", html_build_path)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
